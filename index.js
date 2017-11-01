bf3c458b90be5a33c6c81153320bcc6607afd14f
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2017-11-01 10:25:38 +0900

    apply V8 fast property hack only if needed
    This plugin uses V8 fast property hack https://stackoverflow.com/questions/24987896/, but it's not needed if no properties are deleted.

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-/*!
- * metalsmith-babel | MIT (c) Shinnosuke Watanabe
- * https://github.com/babel/metalsmith-babel
-*/
 'use strict';
 
-const path = require('path');
+const pathLib = require('path');
+
+const dirname = pathLib.dirname;
+const extname = pathLib.extname;
+const join = pathLib.join;
+const relative = pathLib.relative;
 
 const babel = require('babel-core');
 const SafeBuffer = require('safe-buffer').Buffer;
@@ -12,10 +13,11 @@ const toFastProperties = require('to-fast-properties');
 
 module.exports = function metalsmithBabel(options) {
   options = Object.assign({}, options);
+  let noFilesRenamed = true;
 
   return function metalsmithBabelPlugin(files, metalsmith) {
-    Object.keys(files).forEach(originalFilename => {
-      const ext = path.extname(originalFilename).toLowerCase();
+    for (const originalFilename of Object.keys(files)) {
+      const ext = extname(originalFilename).toLowerCase();
       if (ext !== '.js' && ext !== '.jsx') {
         return;
       }
@@ -25,11 +27,11 @@ module.exports = function metalsmithBabel(options) {
       if (originalFilename !== filename) {
         files[filename] = files[originalFilename];
         delete files[originalFilename];
-        toFastProperties(files);
+        noFilesRenamed = false;
       }
 
       const result = babel.transform(String(files[filename].contents), Object.assign({}, options, {
-        filename: path.join(metalsmith.directory(), metalsmith.source(), originalFilename),
+        filename: join(metalsmith.directory(), metalsmith.source(), originalFilename),
         filenameRelative: originalFilename,
         sourceMapTarget: filename
       }));
@@ -40,15 +42,21 @@ module.exports = function metalsmithBabel(options) {
           contents: SafeBuffer.from(JSON.stringify(result.map))
         };
 
-        // https://github.com/babel/babel/blob/v6.14.0/packages/babel-core/src/transformation/file/options/config.js#L123
+        // https://github.com/babel/babel/blob/v6.23.0/packages/babel-core/src/transformation/file/options/config.js#L123
         if (options.sourceMap !== 'both' && options.sourceMaps !== 'both') {
           result.code += `\n//# sourceMappingURL=${
-            path.relative(path.dirname(filename), sourcemapPath).replace(/\\/g, '/')
+            relative(dirname(filename), sourcemapPath).replace(/\\/g, '/')
           }\n`;
         }
       }
 
       files[filename].contents = SafeBuffer.from(result.code);
-    });
+    }
+
+    if (noFilesRenamed) {
+      return;
+    }
+
+    toFastProperties(files);
   };
 };
